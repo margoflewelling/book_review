b9d2b03626ac506731068ec775961bd700e316da
Margo Flewelling <flewelling.margo@gmail.com>
2020-05-29 10:56:16 -0600

    Refactor

--- app/services/nyt_service.rb
+++ app/services/nyt_service.rb
@@ -2,14 +2,18 @@ require 'JSON'
 class NytService
 
   def get_reviews(title)
-    info = Faraday.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{title}") do |f|
-      f.params['api-key'] = ENV['NYT_API_KEY']
-    end
-    json = JSON.parse(info.body, symbolize_names: true)
+    json = JSON.parse(conn(title).body, symbolize_names: true)
     reviews = []
     json[:results].each do |result|
       reviews << {publication_dt: result[:publication_dt], summary: result[:summary]}
     end
     reviews
   end
+
+  def conn(title)
+    Faraday.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{title}") do |f|
+      f.params['api-key'] = ENV['NYT_API_KEY']
+    end
+  end
+
 end
--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -5,10 +5,9 @@ class OpenLibraryService
   def get_book_details(title)
     info = Faraday.get("http://openlibrary.org/search.json?title=#{title}")
     json = JSON.parse(info.body, symbolize_names: true)
-    author = json[:docs].first[:author_name].first
-    title = json[:docs].first[:title]
-    publisher = json[:docs].first[:publisher]
-    {author: author, title: title, publisher: publisher}
+    {author: json[:docs].first[:author_name].first,
+      title: json[:docs].first[:title],
+      publisher: json[:docs].first[:publisher]}
   end
 
 
